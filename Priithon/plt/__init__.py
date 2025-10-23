88a42ecf7cadeca20aa7739fb551027c2f00c1a2
matsuda <a.matsuda@nict.go.jp>
2025-10-23 14:22:02 +0900

    higher numpy version

---
+++ Priithon/plt/__init__.py
@@ -0,0 +1,11 @@
+from .wxplt import *
+from .plot_objects import *
+from .plot_utility import *
+from .interface import *
+
+from . import plot_objects
+from . import plot_utility
+from . import wxplt
+from . import interface
+
+from .wxplt import lena
